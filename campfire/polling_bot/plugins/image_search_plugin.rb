476ac599199e653992492b3a5f481b1e3c1c1baf
Brad Greenlee <brad@wesabe.com>
2008-12-19 00:12:12 -0800

    Better error handling

--- campfire/polling_bot/plugins/image_search_plugin.rb
+++ campfire/polling_bot/plugins/image_search_plugin.rb
@@ -7,11 +7,12 @@ class ImageSearchPlugin < Campfire::PollingBot::Plugin
     case message.command
     when /(?:photo|image|picture)\s+of:?\s+(?:a:?\s+)?\s*("?)(.*?)\1$/i
       subject = $2
-      photo_links = query_flickr(subject)
-      if photo_links.empty?
-        bot.say("Couldn't find anything for \"#{subject}\"")
-      else
-        bot.say_random(photo_links)
+      if photo_links = query_flickr(subject)
+        if photo_links.empty?
+          bot.say("Couldn't find anything for \"#{subject}\"")
+        else
+          bot.say_random(photo_links)
+        end
       end
       return HALT
     end
@@ -37,11 +38,18 @@ class ImageSearchPlugin < Campfire::PollingBot::Plugin
   def query_flickr(subject)
     query = "select * from flickr.photos.search where text=\"#{subject}\""
     res = client.get("http://query.yahooapis.com/v1/public/yql", :q => query, :format => 'json')
-    return [] unless res.status == 200
-    result = JSON.parse(res.content)
-    return [] if result["query"]["count"] == "0"
-    photos = result["query"]["results"]["photo"]
-    photos = [photos] if photos.is_a?(Hash)
-    return photos.map {|p| "http://farm%s.static.flickr.com/%s/%s_%s.jpg?v=0" % [p['farm'], p['server'], p['id'], p['secret']]}
+    case res.status
+    when 200
+      result = JSON.parse(res.content)
+      return [] if result["query"]["count"] == "0"
+      photos = result["query"]["results"]["photo"]
+      photos = [photos] if photos.is_a?(Hash)
+      return photos.map {|p| "http://farm%s.static.flickr.com/%s/%s_%s.jpg?v=0" % [p['farm'], p['server'], p['id'], p['secret']]}
+    when 403
+      bot.say("Sorry, we seem to have hit our query limit for the day.")
+    else
+      bot.say("Hmm...didn't work. Got this response:")
+      bot.paste(result.content)      
+    end
   end
 end
